650dcdd8cbc11774cbeecff427804c370d87f924
manish201258 <manish201258@gmail.com>
2025-05-30 02:34:47 +0530

    code updated

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,12 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
-const corsOptions={
-    origin:'https://jobhub-qw73.onrender.com',
-    credentials:true
-}
 
-
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: '*',
+    credentials: true
+  }));
+  
 const PORT=process.env.PORT || 3000;
   
 
