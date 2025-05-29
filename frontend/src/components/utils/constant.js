e2213a5331cb9cfd56b8501aca295f1a4e2239a8
manish201258 <manish201258@gmail.com>
2025-05-30 02:27:39 +0530

    code updated

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://jobhub-0jq5.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://jobhub-0jq5.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobhub-0jq5.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobhub-0jq5.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://jobhub-qw73.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobhub-qw73.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobhub-qw73.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="hhttps://jobhub-qw73.onrender.com/api/v1/company";
